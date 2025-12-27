7c5299867d70cbf07e0c264e4684063103a7dbdd
Erik Haake <ehaake@pdx.edu>
2025-12-27 15:35:49 -0800

    Get drawing text in StartMenu working

--- src/screen.rs
+++ src/screen.rs
@@ -22,11 +22,12 @@ pub struct MenuState {
 
 impl Screen {
     pub fn draw(&self, frame: &mut Frame, config: &Config) {
-
         // app call board.draw() so do nothing if InGame
         match self {
             Screen::StartMenu { menu_state } => menu_state.draw(frame, config),
-            Screen::InGame { game_state: _game_state } => {}
+            Screen::InGame {
+                game_state: _game_state,
+            } => {}
         }
     }
 }
@@ -39,12 +40,24 @@ impl MenuState {
         }
     }
 
-    pub fn draw(&self, frame: &mut Frame, config: &Config) {
-        // let mid = config.num_cols / 2;
-        
+    // Draw Text Helper
+    //
+    fn draw_text(&self, text: &str, x: usize, y: usize, frame: &mut Frame) {
+        for (i, ch) in text.chars().enumerate() {
+            frame[x + i][y] = ch;
+        }
     }
 
-    /// 
+    pub fn draw(&self, frame: &mut Frame, config: &Config) {
+        let text = "Kaazap!";
+        let mid = config.num_cols / 2;
+        let padding_x = text.len() / 2;
+        let padding_y = 15;
+
+        self.draw_text(text, mid - padding_x, padding_y, frame);
+    }
+
+    ///
     pub fn tick(&mut self, dt: Duration) {
         self.time_accumulated += dt;
         if self.time_accumulated >= Duration::from_millis(350) {
@@ -52,6 +65,11 @@ impl MenuState {
             // anim_state.toggle();
             self.time_accumulated -= Duration::from_millis(350);
         }
+    }
+}
 
+impl Default for MenuState {
+    fn default() -> Self {
+        Self::new()
     }
 }
